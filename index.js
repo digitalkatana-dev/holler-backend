77cb0c437a51b24ebd917ab3966a39cff533687c
nucleusdev01 <dev@nucleusinsurance.com>
2024-01-31 16:53:18 -0800

    updated client origin url

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: 'http://localhost:3000',
+		origin: 'https://holler.onrender.com',
 		methods: ['GET', 'POST'],
 	},
 	pingTimeout: 60000,
